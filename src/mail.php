ef124ee3b425c47fe2ba0dbdad6e089aa4fe4aa2
Ekaterina <biruta78@gmail.com>
2019-06-23 23:27:09 +0500

    добавлена форма обратной связи

--- src/mail.php
+++ src/mail.php
@@ -1,5 +1,15 @@
 <?php
 
-$data = $_POST;
+//$data = $_POST;
 //echo json_encode($data,JSON_UNESCAPED_UNICODE);
-echo $data['username'] . ', Ваша форма отправлена!';
+//echo $data['username'] . ', Ваша форма отправлена!';
+
+/*$mail->Charset = 'utf-8';
+
+$name = $_POST['username'];
+$phone = $_POST['phone'];
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth = true;
+*/
